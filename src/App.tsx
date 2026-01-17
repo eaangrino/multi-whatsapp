faa015397377643107965adde64b179cca090767
eaangrino <eaangrino@gmail.com>
2026-01-17 12:37:18 -0500

    delete import fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { MdDelete } from "react-icons/md";
 import { FaWhatsapp } from "react-icons/fa";
 
