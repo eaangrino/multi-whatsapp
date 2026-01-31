8096d8136f5f4bfa2c89cdde59ecf2cea4e0fbb7
eaangrino <eaangrino@gmail.com>
2026-01-31 18:52:49 -0500

    added packages required for project in nixos

--- shell.nix
+++ shell.nix
@@ -1,6 +1,42 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
-  packages = [
-    pkgs.nodejs_22
+  packages = with pkgs; [
+    nodejs_22
+
+    # Electron runtime deps
+    gtk3
+    glib
+    nss
+    nspr
+    atk
+    at-spi2-atk
+    cups
+    libdrm
+    mesa
+    libxkbcommon
+    alsa-lib
+    dbus
+    pango
+    cairo
+    expat
+    dbus
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXdamage
+    xorg.libXext
+    xorg.libXfixes
+    xorg.libXi
+    xorg.libXrandr
+    xorg.libXScrnSaver
+    xorg.libXtst
   ];
+
+  shellHook = ''
+    export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
+      pkgs.glib
+      pkgs.gtk3
+      pkgs.nss
+      pkgs.nspr
+    ]}
+  '';
 }
