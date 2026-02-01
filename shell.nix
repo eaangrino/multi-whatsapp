1e3218ce5d56601daa29599e6cf0c9844fa97b35
eaangrino <eaangrino@gmail.com>
2026-01-31 19:52:19 -0500

    fix run electron in nixos

--- shell.nix
+++ shell.nix
@@ -1,61 +1,15 @@
 { pkgs ? import <nixpkgs> {} }:
+
 pkgs.mkShell {
   packages = with pkgs; [
-    nodejs_20
+    nodejs_22
+    yarn
     electron
-    # Electron runtime deps
-    gtk3
-    glib
-    nss
-    nspr
-    atk
-    at-spi2-atk
-    cups
-    libdrm
-    mesa
-    libxkbcommon
-    alsa-lib
-    dbus
-    pango
-    cairo
-    expat
-    xorg.libX11
-    xorg.libXcursor
-    xorg.libXdamage
-    xorg.libXext
-    xorg.libXfixes
-    xorg.libXi
-    xorg.libXrandr
-    xorg.libXScrnSaver
-    xorg.libXtst
   ];
 
-  shellHook = ''
-    export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
-      pkgs.dbus
-      pkgs.glib
-      pkgs.gtk3
-      pkgs.atk
-      pkgs.pango
-      pkgs.cairo
-      pkgs.cups
+  # binario correcto
+  ELECTRON_PATH = "${pkgs.electron}/bin/electron";
 
-      pkgs.libdrm
-      pkgs.mesa
-
-      pkgs.xorg.libX11
-      pkgs.xorg.libXcursor
-      pkgs.xorg.libXdamage
-      pkgs.xorg.libXext
-      pkgs.xorg.libXfixes
-      pkgs.xorg.libXi
-      pkgs.xorg.libXrandr
-      pkgs.xorg.libXScrnSaver
-      pkgs.xorg.libXtst
-      pkgs.xorg.libXcomposite
-
-      pkgs.nss
-      pkgs.nspr
-    ]}
-  '';
+  # directorio BASE donde existe dist/
+  ELECTRON_OVERRIDE_DIST_PATH = "${pkgs.electron}/bin";
 }
